c69eee5072accbfa9f270e9ff556011e826e9ce1
david <davidjuannn@gmail.com>
2018-10-11 15:27:22 +0200

    Primera

---
+++ num500.000.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        Request
+    </head>
+    <body>
+        <form action="num500.000.php" method="post">
+            <input name="numero" type="number">
+            <input type="submit" value="submit">
+        </form>
+        <?php
+        $num=$_POST['numero'];
+        for($i=0;$i<500000;$i++){
+            $aleatori= rand(0,500000);
+            if($aleatori==$num){
+                list($useg, $seg) = explode(" ", microtime());
+            echo ((float)$useg + (float)$seg."milisegons");
+                exit;
+            }
+        }
+        echo("valor no trobat");
+    ?>
+    </body>
+</html>
